db2d61dec987ecae579d67d6c0519f54a02dc14b
Kashish <kash26ish@gmail.com>
2026-06-05 16:28:58 +0530

    Create problem_02.java

---
+++ Week_1/Day_5/problem_02.java
@@ -0,0 +1,18 @@
+//reverse string
+
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int lp = 0;
+        int rp = s.length-1;
+        while(lp < rp) {
+            //swap
+            char temp = s[lp];
+            s[lp] = s[rp];
+            s[rp] = temp;
+
+            lp++;
+            rp--;
+        }
+    }
+}
