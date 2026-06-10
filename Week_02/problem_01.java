15d901c63200a07c4553e6271c719272f06872c2
Kashish <kash26ish@gmail.com>
2026-06-10 13:44:33 +0530

    Create problem_01.java

---
+++ Week_02/problem_01.java
@@ -0,0 +1,21 @@
+//Find the index of the first occurence in a string
+//implement strStr()
+class Solution {
+    public int strStr(String haystack, String needle) {
+    
+        int n = haystack.length();
+        int m = needle.length();
+
+        if(n < m) {
+            return -1;
+        }
+
+        for(int i=0; i<=n-m; i++) {
+            if(haystack.substring(i, i+m).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
