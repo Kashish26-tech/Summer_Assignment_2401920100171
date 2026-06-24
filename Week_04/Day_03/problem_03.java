4c377ed0b5ba4899a7319c5962527162fb044fd7
Kashish <kash26ish@gmail.com>
2026-06-24 15:27:33 +0530

    Create problem_03.java

---
+++ Week_04/Day_03/problem_03.java
@@ -0,0 +1,29 @@
+// lowest common anscestor in bst
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode leftLca = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightLca = lowestCommonAncestor(root.right, p, q);
+
+        if(rightLca == null) {
+            return leftLca;
+        }
+        if(leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+}
