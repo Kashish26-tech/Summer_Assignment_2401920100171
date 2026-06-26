aee4e59b01dc4e930c0c74ff6a5429b285ae5c93
Kashish <kash26ish@gmail.com>
2026-06-26 16:30:05 +0530

    Create problem_02.java

---
+++ Week_04/Day_05/problem_02.java
@@ -0,0 +1,44 @@
+//Construct binary tree from preorder and postorder
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int preIndex = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(preorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode helper(int[] preorder, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = map.get(rootVal);
+
+        root.left = helper(preorder, start, index - 1);
+        root.right = helper(preorder, index + 1, end);
+
+        return root;
+    }
+}
